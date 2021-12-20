8b183d0d3581ad07048eaa7f0e44bcc9f8abc3b6
ParamagicDev <konnor5456@gmail.com>
2021-12-20 01:37:31 -0500

    fix csp, and then add translations

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,26 +4,26 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https, :http
-  policy.font_src    :self, :https, :data, :http
-  policy.img_src     :self, :https, :data, :http
-  policy.object_src  :none
-  policy.script_src  :self, :https, :http
-  # Allow @vite/client to hot reload changes in development
-  policy.script_src *policy.script_src, :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
+# Rails.application.config.content_security_policy do |policy|
+#   # policy.default_src :self, :https
+#   # policy.font_src    :self, :https, :data
+#   # policy.img_src     :self, :https, :data
+#   # policy.object_src  :none
+#   # policy.script_src  :self, :https, :http
+#   # policy.style_src   :self, :https
+#   # policy.connect_src :self, :https
 
-  # You may need to enable this in production as well depending on your setup.
-  policy.script_src *policy.script_src, :blob if Rails.env.test?
+#   if Rails.env.development? || Rails.env.test?
+#     srcs = [
+#       "http://#{ ViteRuby.config.host_with_port }",
+#       "ws://#{ ViteRuby.config.host_with_port }",
+#     ]
+#     # Allow @vite/client to hot reload changes in development
+#     policy.script_src :unsafe_eval, *srcs
 
-  policy.style_src   :self, :https, :http
-  policy.connect_src :self, :https, :http
-  # Allow @vite/client to hot reload changes in development
-  policy.connect_src *policy.connect_src, "ws://#{ ViteRuby.config.host_with_port }" if Rails.env.development?
-
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
-end
+#     # Allow @vite/client to hot reload changes in development
+#     policy.connect_src *srcs
+#   end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
@@ -35,3 +35,21 @@ end
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src :self, :https, :data
+  policy.img_src :self, :https, :data
+  policy.object_src :none
+  policy.script_src :self, :https
+  policy.style_src :self, :https, :unsafe_inline
+  policy.connect_src :self, :https, :data
+
+  if Rails.env.development? || Rails.env.test?
+    # Allow @vite/client to hot reload changes in development
+    policy.script_src :unsafe_eval, "http://#{ ViteRuby.config.host_with_port }", *policy.script_src
+
+    # Allow @vite/client to hot reload changes in development
+    policy.connect_src "ws://#{ ViteRuby.config.host_with_port }", *policy.connect_src
+  end
+# policy.report_uri "/csp-violation-report-endpoint"
+end
