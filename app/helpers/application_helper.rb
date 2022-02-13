8fa6a6917c5b14b28a6d36e5e074b37ac586fa80
ParamagicDev <konnor5456@gmail.com>
2022-02-12 20:30:51 -0500

    working on cleaning things up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  # Returns a +sl-+ prefixed element.
+  # @param {#to_s} name - The name of the element without "sl-"
+  # @return {String}
+  # @example
+  #   <%= sl_tag "input", name: "user[name]", id: "user_name" %>
   def sl_tag(name, **options, &block)
     tag.public_send("sl_#{name.to_s.underscore}".to_sym, **options, &block)
   end
@@ -18,29 +23,62 @@ module ApplicationHelper
     sl_tag("input", type: "password", **options, &block)
   end
 
-  def fetch_and_set(hash, key, default_value)
-    hash[key.to_sym] = hash.fetch(key, default_value)
-  end
-
   ## Helpers for using non-standard form elements.
+
+  # Generates a form id for a given Object + method
+  # @param {Object} resource - An instance of an ActiveRecord or ActiveModel usually.
+  # @param {Symbol} symbol - The attribute to call.
+  # @return {String}
+  # @example
+  #   <% user = User.new %>
+  #   <%= form_with model: user do %>
+  #     <input id="<%= form_id(user, :first_name)" %>" name="user[first_name]" />
+  #   <% end %>
   def form_id(resource, symbol)
     "#{resource.class.name.underscore}_#{symbol}"
   end
 
+  # Generates a form name for a given Object + method
+  # @param {Object} resource - An instance of an ActiveRecord or ActiveModel usually.
+  # @param {Symbol} symbol - The attribute to call.
+  # @return {String}
+  # @example
+  #   <% user = User.new %>
+  #   <%= form_with model: user do %>
+  #     <input id="user_first_name" name="<%= form_name(user, :first_name) %>" />
+  #   <% end %>
   def form_name(resource, symbol)
     "#{resource.class.name.underscore}[#{symbol}]"
   end
 
+  # Generates a form name for a given Object + method
+  # @param {Object} resource - An instance of an ActiveRecord or ActiveModel usually.
+  # @param {Symbol} symbol - The attribute to call.
+  # @return {String}
+  # @example
+  #   <% user = User.new %>
+  #   <%= form_with model: user do %>
+  #     <input id="user_first_name" name="user[first_name]" value="<%= form_value(user, :first_name) %>" />
+  #   <% end %>
   def form_value(resource, symbol)
     resource.public_send(symbol.to_sym)
   end
 
+  # The string version of id, name, and value of an Object.
+  # @param {Object} resource - An instance of an ActiveRecord or ActiveModel usually.
+  # @param {Symbol} symbol - The attribute to call.
+  # @return {String}
   # @example
   #   <sl-input <%= form_fields_string(resource, :column) %>></sl-input>
   def form_fields_string(resource, symbol)
     form_fields(resource, symbol).map { |key, value| "#{key}='#{value}'" }.join(" ")
   end
 
+
+  # The Hash version of id, name, and value of an Object.
+  # @param {Object} resource - An instance of an ActiveRecord or ActiveModel usually.
+  # @param {Symbol} symbol - The attribute to call.
+  # @return {Hash{Symbol => String}}
   # @example
   #   <%= sl_tag "input", **form_fields(resource, :column) %>
   def form_fields(resource, symbol)
