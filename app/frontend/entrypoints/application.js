b08b52cac6cc89a31ef4679ce25873fd18040b4b
ParamagicDev <konnor5456@gmail.com>
2022-03-13 13:51:39 -0400

    tidy up code

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -3,56 +3,8 @@ import Rails from "mrujs"
 import "@hotwired/turbo"
 import "../channels"
 import "../controllers"
+import { asPromise, preventDoubleClick } from "../src"
 import { componentPromises } from "../components"
-
-function clickEventIsSignificant(event) {
-  return !(
-    (event.target && event.target.isContentEditable)
-    || event.defaultPrevented
-    || event.which > 1
-    || event.altKey
-    || event.ctrlKey
-    || event.metaKey
-    || event.shiftKey
-  )
-}
-
-function findSubmitButton(target) {
-  if (target instanceof HTMLElement) {
-    return  target.closest("sl-button[type='submit']:not([disabled]):not(:disabled)")
-  }
-
-  if (Array.isArray(target)) {
-    return target.find((el) => {
-      return el.type === "submit" && el.localName === "sl-button"
-    })
-  }
-};
-
-function asPromise (callback, ...args) {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(callback(...args))
-    } catch(e) {
-      reject(e)
-    }
-  })
-}
-
-function preventDoubleClick () {
-  window.addEventListener("click", (event) => {
-    if (!clickEventIsSignificant(event)) return
-    const target = (event.composedPath && event.composedPath()) || event.target
-
-    const submitBtn = findSubmitButton(target)
-    if (submitBtn == null) return
-    if (submitBtn.hasAttribute("loading")) return
-
-    submitBtn.setAttribute("loading", "")
-    submitBtn.formSubmitController.submit()
-
-  }, { capture: true })
-}
 
 ;(async function () {
   // Import all images
@@ -63,7 +15,7 @@ function preventDoubleClick () {
     // ...shoelacePromises,
     componentPromises(),
     asPromise(preventDoubleClick),
-    asPromise(Rails.start.bind(Rails), { errorRenderer: "turbo" }),
+    asPromise(Rails.start.bind(Rails)),
     asPromise(ActiveStorage.start),
   ]);
 })();
---
+++ app/frontend/src/index.js
@@ -0,0 +1,49 @@
+export function asPromise (callback, ...args) {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(callback(...args))
+    } catch(e) {
+      reject(e)
+    }
+  })
+}
+
+export function preventDoubleClick () {
+  window.addEventListener("click", (event) => {
+    if (!clickEventIsSignificant(event)) return
+    const target = (event.composedPath && event.composedPath()) || event.target
+
+    const submitBtn = findSubmitButton(target)
+    if (submitBtn == null) return
+    if (submitBtn.hasAttribute("loading")) return
+
+    submitBtn.setAttribute("loading", "")
+    submitBtn.formSubmitController.submit()
+
+  }, { capture: true })
+}
+
+function clickEventIsSignificant(event) {
+  return !(
+    (event.target && event.target.isContentEditable)
+    || event.defaultPrevented
+    || event.which > 1
+    || event.altKey
+    || event.ctrlKey
+    || event.metaKey
+    || event.shiftKey
+  )
+}
+
+function findSubmitButton(target) {
+  if (target instanceof HTMLElement) {
+    return  target.closest("sl-button[type='submit']:not([disabled]):not(:disabled)")
+  }
+
+  if (Array.isArray(target)) {
+    return target.find((el) => {
+      return el.type === "submit" && el.localName === "sl-button"
+    })
+  }
+};
+
